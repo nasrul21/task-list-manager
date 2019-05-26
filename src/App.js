b8a36e2fd16c2fe02e1190ffdf2adb5b4e1a2a07
Nasrul Faizin <faizinnasrul21@gmail.com>
2019-05-26 12:47:52 +0700

    chrone(view) add task list view & style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    tasks: [
+      { text: "Bangun", done: true },
+      { text: "Sahur", done: true },
+      { text: "Masak", done: false },
+      { text: "Makan", done: false },
+      { text: "Beres-beres", done: false },
+      { text: "Ibadah", done: false }
+    ]
+  }
+
+  renderItem() {
+    return this.state.tasks.map((item) => (
+        // <li className={item.done ? "checked" : ""}>
+        //   <span className="item-content">{item.text}</span>
+        //   <span className="item-action">
+        //     <button className="btn green">Done</button>
+        //   </span>
+        // </li>
+        <li className={"online ${item.done ? 'checked' : ''}"}>
+          {item.text}
+          <span className="action">
+            <button className="btn green">Done</button>
+            <button className="btn blue">Edit</button>
+            <button className="btn red">X</button>
+          </span>
+        </li>
+    ))
+  }
+
+  render() {
+    return (
+      <div className="App">      
+        <form className="header">
+          <h2>Task List Manager</h2>
+          <input type="text" id="myInput" placeholder="Title..." />
+          <button type="submit" className="btn addBtn">Add</button>
+        </form>
+
+        <ul>
+          {this.renderItem()}
+        </ul> 
+      </div>
+    );
+  }
 }
 
 export default App;
