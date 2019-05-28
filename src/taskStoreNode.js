f36a1402a0aadfd172d4e418c04e77047463be8e
Nasrul Faizin <faizinnasrul21@gmail.com>
2019-05-28 21:18:12 +0700

    chrone(db) add task store/model nodejs version

---
+++ src/taskStoreNode.js
@@ -0,0 +1,27 @@
+const PouchyStore =  require('../pouchy-store-node/PouchyStore');
+
+class TaskStoreNode extends PouchyStore {
+
+  get name() {
+    return this._name;
+  }
+
+  setName(dbName) {
+    this._name = dbName;
+  }
+
+  get urlRemote() {
+    return "http://13.250.43.79:5984";
+  }
+
+  get optionsRemote() {
+    return {
+      auth: {
+	    username: 'admin',
+	    password: 'iniadmin',
+	  }
+    };
+  }
+}
+
+module.exports = new TaskStoreNode();
