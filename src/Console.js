5135fbe82dbf833169327bbada83d0d806aa7203
Nasrul Faizin <faizinnasrul21@gmail.com>
2019-05-28 21:58:11 +0700

    feat(console) update task done/complete

--- src/Console.js
+++ src/Console.js
@@ -1,6 +1,13 @@
 const inquirer = require('inquirer');
 const taskStore = require('./taskStoreNode');
 
+// action key
+const ACTION = {
+    DONE: "is done",
+    EDIT: "edit",
+    DELETE: "delete"
+}
+
 async function initialize() {
     console.log('popup initialize all offline data...');
     taskStore.setName("efishery_tasks");
@@ -11,6 +18,12 @@ async function initialize() {
 initialize().then(function() {
     showTasks();
 });
+
+// parse selected tasks
+function getResult(result) {
+    const [_, obj] = result.split("|");
+    return JSON.parse(obj);
+}
 
 // Show All Task
 function showTasks() {
@@ -23,14 +36,37 @@ function showTasks() {
                     const { _id, text, done} = item
                     return `${index + 1}. ${taskStore.checkIsUploaded(item) ? "(on )" : "(off)"} | ` + JSON.stringify({ _id, text, done }) 
                 }), new inquirer.Separator()]
+            },
+            {
+                type: 'list',
+                name: 'action',
+                message: function(a) { return `Action for tasks '${getResult(a.tasks).text}'`; },
+                choices: function(a) { 
+                    // if tasks already done, disable 'is done menu'
+                    const isDone = getResult(a.tasks).done;
+                    return Object.values(ACTION).filter(function(item) { return isDone ? item != ACTION.DONE : item })
+                },
             }
         ]).then(answers => {
-            console.log(getResult(answers.tasks));
+            const task = getResult(answers.tasks);
+            switch (answers.action) {
+                case ACTION.DONE: {
+                    console.log("DONE");
+                    return updateTaskDone(task);
+                }
+                default:
+                    console.log("DEFAULT");
+                break;
+            }
             process.exit();
         });
 }
 
-function getResult(result) {
-    const [_, obj] = result.split("|");
-    return JSON.parse(obj);
+// task done action
+async function updateTaskDone(task) {
+    await taskStore.editItem(task._id, {
+        ...task,
+        done: !task.done
+    });
+    console.log(`SUCCESS: Tasks '${task.text}' done!`);
 }
